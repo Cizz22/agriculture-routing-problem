f29e6662a3bd56b0cfe01370ad99151363602c39
Cizz22 <cisatraa@gmail.com>
2023-05-21 21:43:31 +0700

    add fastapi framework

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
 
 """Main file for the project."""
 
-
 from arpga.core import run_arpga
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-def main():
-    ''''main()'''
 
-    result = run_arpga(
-        track=20,
-        width=2.5,
-        radius=3.5,
-        maxGen=200,
-        pSize=100,
-        runNumbers=10
-    )
-
-    print(result)
+@app.get("/")
+async def read_root():
+    return {
+        "status": "Server is running.",
+        "message": "Hello this is the root of the server."
+        
+    }
     
-main()
+    
+@app.post("/runarp")
+async def run_arp(track: int, width: float, radius: float, maxGen: int, pSize: int, runNumbers: int):
+    
+    result = run_arpga(
+        track=track,
+        width=width,
+        radius=radius,
+        maxGen=maxGen,
+        pSize=pSize,
+        runNumbers=runNumbers
+    )
+    
+    return {
+        "success": True,
+        "message": "ARP Genetic Algorithm has been run successfully.",
+        "data": result,
+    }
